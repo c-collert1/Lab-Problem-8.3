91b6e7e847732e8dc536c836d0511afd11ed5d21
Caleb Collert <c-collert.1@onu.edu>
2024-04-02 20:52:51 -0400

    completed convertRomanToInt function

--- P8-3/P8-3.cpp
+++ P8-3/P8-3.cpp
@@ -1,36 +1,76 @@
 #include <iostream>
 using namespace std;
 
-int romanCharValue(char r);
+int convertRomanToInt(string s);
+int romanCharValue(string r);
 
 int main() {
-
-	cout << romanCharValue('I');
+    cout << convertRomanToInt("MCMLXII");
+    return 0;
 }
 
-int romanCharValue(char r) {
+int convertRomanToInt(string s) {
+    int result = 0;
 
-	int romanNumToDecimal = 0;
-	if ( r == 'I') {
-		romanNumToDecimal = 1;
-	}
-	else if (r == 'V') {
-		romanNumToDecimal = 5;
-	}
-	else if (r == 'X') {
-		romanNumToDecimal = 10;
-	}
-	else if (r == 'L') {
-		romanNumToDecimal = 50;
-	}
-	else if (r == 'C') {
-		romanNumToDecimal = 100;
-	}
-	else if (r == 'D') {
-		romanNumToDecimal = 500;
-	}
-	else if (r == 'M') {
-		romanNumToDecimal = 1000;
-	}
-	return romanNumToDecimal;
+    while (s.length() > 0) {
+   
+        string firstTwo = s.substr(0, 2);
+
+        if (firstTwo == "IV") {
+            result += 4;
+            s = s.substr(2);
+        }
+        else if (firstTwo == "IX") {
+            result += 9;
+            s = s.substr(2);
+        }
+        else if (firstTwo == "XL") {
+            result += 40;
+            s = s.substr(2);
+        }
+        else if (firstTwo == "XC") {
+            result += 90;
+            s = s.substr(2);
+        }
+        else if (firstTwo == "CD") {
+            result += 400;
+            s = s.substr(2);
+        }
+        else if (firstTwo == "CM") {
+            result += 900;
+            s = s.substr(2);
+        }
+        else {
+           
+            result += romanCharValue(s.substr(0, 1));
+            s = s.substr(1); 
+        }
+    }
+    return result;
+}
+
+int romanCharValue(string r) {
+    int romanNumToDecimal = 0;
+    if (r == "I") {
+        romanNumToDecimal = 1;
+    }
+    else if (r == "V") {
+        romanNumToDecimal = 5;
+    }
+    else if (r == "X") {
+        romanNumToDecimal = 10;
+    }
+    else if (r == "L") {
+        romanNumToDecimal = 50;
+    }
+    else if (r == "C") {
+        romanNumToDecimal = 100;
+    }
+    else if (r == "D") {
+        romanNumToDecimal = 500;
+    }
+    else if (r == "M") {
+        romanNumToDecimal = 1000;
+    }
+    return romanNumToDecimal;
 }
