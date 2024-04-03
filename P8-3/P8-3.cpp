7b07b9b2918f3f3ca9ce137dba45adbb144fd767
Caleb Collert <c-collert.1@onu.edu>
2024-04-02 20:06:07 -0400

    completed romanCharValue function

--- P8-3/P8-3.cpp
+++ P8-3/P8-3.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
 using namespace std;
 
+int romanCharValue(char r);
+
 int main() {
 
+	cout << romanCharValue('I');
+}
 
+int romanCharValue(char r) {
+
+	int romanNumToDecimal = 0;
+	if ( r == 'I') {
+		romanNumToDecimal = 1;
+	}
+	else if (r == 'V') {
+		romanNumToDecimal = 5;
+	}
+	else if (r == 'X') {
+		romanNumToDecimal = 10;
+	}
+	else if (r == 'L') {
+		romanNumToDecimal = 50;
+	}
+	else if (r == 'C') {
+		romanNumToDecimal = 100;
+	}
+	else if (r == 'D') {
+		romanNumToDecimal = 500;
+	}
+	else if (r == 'M') {
+		romanNumToDecimal = 1000;
+	}
+	return romanNumToDecimal;
 }
